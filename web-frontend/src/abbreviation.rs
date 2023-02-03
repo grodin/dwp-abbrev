3fefc84d4a2b242f807887708744aef4d3a97024
Joseph Cooper <grodin@users.noreply.github.com>
2023-02-03 19:12:06 +0000

    Rewrite it in Rust (#16)
    * feat: Create basic version in rust
    
    * test: Add wasm-bindgen-test as dev dependency
    
    * build: Add rust-toolchain file
    
    * ci: Create build and deploy workflow
    
    ---------
    
    Co-authored-by: Joseph Cooper <cooper.joseph@gmail.com>

---
+++ web-frontend/src/abbreviation.rs
@@ -0,0 +1,27 @@
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Entry {
+    abbr: String,
+    desc: String,
+}
+
+impl Entry {
+    pub fn new(abbr: String, desc: String) -> Self {
+        Self { abbr, desc }
+    }
+
+    pub fn abbreviation(&self) -> &str {
+        &self.abbr
+    }
+
+    pub fn description(&self) -> &str {
+        &self.desc
+    }
+}
+
+pub static ABBREVIATIONS: Lazy<Vec<Entry>> = Lazy::new(|| {
+    let json = include_str!("abbreviations.json");
+    serde_json::from_str(json).expect("Failed to parse abbreviations from JSON")
+});
