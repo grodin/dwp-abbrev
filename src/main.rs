520371d56b05bfde9e86fdd5cad6ff569dda9abf
Joseph Cooper <grodin@users.noreply.github.com>
2023-03-31 12:37:55 +0100

    feat: Focus filter input when window gains focus
    Closes: #21

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,25 @@ fn app() -> Element {
         .take(70)
         .collect();
 
+    const FILTER_INPUT_ID: &str = "filter_input";
+
+    // Call out to JS to set the window onfocus event.
+    // Trying to do so via web-sys is too painful and overkill for this tiny use case.
+    use_effect(move || {
+        document::eval(&format!(
+            r#"
+            window.onfocus = (_event) => {{
+                const filter_input = document.getElementById("{FILTER_INPUT_ID}")
+                filter_input.focus()
+                filter_input.select()
+            }}
+        "#
+        ));
+    });
     rsx! {
         div {
             input {
+                id: FILTER_INPUT_ID,
                 value: "{filter_query}",
                 r#type: "text",
                 oninput: move |e| filter_query.set(e.value().as_str().into()),
