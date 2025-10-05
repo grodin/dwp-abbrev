67e0f52dbac886fb8b0d0bfd7a0f22d4d6e9e348
Joseph Cooper <cooper.joseph@gmail.com>
2025-10-05 15:37:35 +0100

    feat: Escape key resets

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use crate::abbreviation::{Entry, ABBREVIATIONS};
 use crate::filter::Filter;
+use crate::on_event::use_on_window;
 use dioxus::prelude::*;
+use std::rc::Rc;
 
 mod abbreviation;
 mod filter;
+mod on_event;
 
 fn main() {
     console_error_panic_hook::set_once();
@@ -12,6 +15,8 @@ fn main() {
 
 fn app() -> Element {
     let mut filter_query = use_signal(|| Filter::from(""));
+
+    let mut filter_query_element: Signal<Option<Rc<MountedData>>> = use_signal(|| None);
 
     let mut entries: Vec<&Entry> = ABBREVIATIONS
         .iter()
@@ -30,30 +35,46 @@ fn app() -> Element {
     use_effect(move || {
         document::eval(&format!(
             r#"
+            const filter_input = document.getElementById("{FILTER_INPUT_ID}")
             window.onfocus = (_event) => {{
-                const filter_input = document.getElementById("{FILTER_INPUT_ID}")
                 filter_input.focus()
+                filter_input.select()
+            }}
+            filter_input.onfocus = (_event) => {{
                 filter_input.select()
             }}
         "#
         ));
     });
-    rsx! {
-        div {
-            input {
-                id: FILTER_INPUT_ID,
-                value: "{filter_query}",
-                r#type: "text",
-                oninput: move |e| filter_query.set(e.value().as_str().into()),
-                autofocus: true
+
+    use_on_window("keydown", move |event: web_sys::KeyboardEvent| {
+        if event.key() == "Escape" {
+            filter_query.set(Filter::from(""));
+            if let Some(filter_query_element) = filter_query_element.cloned() {
+                let _ignored = filter_query_element.set_focus(true);
             }
         }
+    });
 
-        table {
-            for entry in entries.iter() {
-                tr {
-                    td { "{entry.abbreviation()}" }
-                    td { "{entry.description()}" }
+    rsx! {
+        div { id: "container", tabindex: 0,
+            div {
+                input {
+                    id: FILTER_INPUT_ID,
+                    value: "{filter_query}",
+                    r#type: "text",
+                    oninput: move |e| filter_query.set(e.value().as_str().into()),
+                    autofocus: true,
+                    onmounted: move |elt| filter_query_element.set(Some(elt.data))
+                }
+            }
+
+            table {
+                for entry in entries.iter() {
+                    tr {
+                        td { "{entry.abbreviation()}" }
+                        td { "{entry.description()}" }
+                    }
                 }
             }
         }
